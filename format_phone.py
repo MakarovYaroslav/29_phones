a37f051f5030945d50bf8a03fc848efb37416719
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2018-01-17 13:10:27 +0300

    Изменено форматирование номера

--- format_phone.py
+++ format_phone.py
@@ -8,23 +8,7 @@ def remove_punctuation(phone_number):
     return number_without_punctuation
 
 
-def is_numeric(phone_number):
-    digits_re = re.compile(r'^\d+$')
-    return bool(digits_re.match(phone_number))
-
-
 def remove_country_code(phone_number):
-    if phone_number.startswith('+'):
-        if phone_number.startswith('+7'):
-            number_without_code = phone_number[2:]
-            return number_without_code
-        else:
-            return None
-    else:
-        return phone_number
-
-
-def truncate_number_length(phone_number):
     phonenumber = phonenumbers.parse(phone_number, "RU")
     if phonenumbers.is_valid_number(phonenumber):
         national_number = str(phonenumber.national_number)
@@ -42,7 +26,4 @@ def format_phone_number(phone_number):
     number = remove_country_code(number)
     if number is None:
         return None
-    if not is_numeric(number):
-        return None
-    number = truncate_number_length(number)
     return number
