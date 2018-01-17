9d312e92146996242b7c41bff95749f0fc80d0f3
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2018-01-17 18:24:09 +0300

    Добавлена защита от потери соединения с БД

--- updating_db.py
+++ updating_db.py
@@ -3,6 +3,8 @@ from models import db, Orders
 from os import getenv
 from format_phone import format_phone_number
 import time
+import psycopg2
+from sqlalchemy.exc import OperationalError
 
 app = Flask(__name__)
 
@@ -22,15 +24,20 @@ db.init_app(app)
 
 if __name__ == "__main__":
     last_order_id = 0
-    timeout = 120
+    timeout = 2
     while True:
         with app.app_context():
-            orders = Orders.query.all()[last_order_id:]
-            if len(orders) != 0:
-                for order in orders:
-                    number = order.contact_phone
-                    new_number = format_phone_number(order.contact_phone)
-                    order.formatted_phone = new_number
-                db.session.commit()
-                last_order_id = orders[-1].id
-            time.sleep(timeout)
+            try:
+                orders = Orders.query.all()[last_order_id:]
+                if len(orders) != 0:
+                    for order in orders:
+                        number = order.contact_phone
+                        new_number = format_phone_number(number)
+                        print(number, new_number)
+                        order.formatted_phone = new_number
+                    db.session.commit()
+                    last_order_id = orders[-1].id
+            except (psycopg2.OperationalError, OperationalError) as e:
+                pass
+            finally:
+                time.sleep(timeout)
