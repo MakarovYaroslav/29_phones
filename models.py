5dfe0481addb694b2c820c52a9923ddd3ca9fdf0
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2018-01-17 13:14:14 +0300

    Убраны неиспользуемые поля

--- models.py
+++ models.py
@@ -5,12 +5,5 @@ db = SQLAlchemy()
 
 class Orders(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    contact_name = db.Column(db.String(200))
     contact_phone = db.Column(db.String(100))
     formatted_phone = db.Column(db.String(20))
-    contact_email = db.Column(db.String(100))
-    status = db.Column(db.Integer)
-    created = db.Column(db.DateTime, index=True)
-    confirmed = db.Column(db.DateTime, index=True)
-    comment = db.Column(db.Text)
-    price = db.Column(db.Numeric)
